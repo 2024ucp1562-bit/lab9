7935aeb01507670093c9bc0e6c613b99026cc586
mnit <2024ucp1562@mnit.ac.in>
2025-11-04 14:31:18 +0530

    vecAdd.cpp added

---
+++ vecAdd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vec={1,5,6,9,8,0,54,5,21,1};
+    int ans=0;
+    for(int it: vec){
+        ans+=it;
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
